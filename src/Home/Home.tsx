75e54cfb852686c6d2ba0174105e2a4c4d6a60da
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-05-11 01:04:33 +0600

    Feature : Brainstormed for basic UI designs

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import Navbar from './Navbar.tsx';
 
 const Home = () => {
   return (
-    <div className="bg-[#F5EFE6] min-h-screen" >
-      <header></header>
-      <main className="h-screen flex justify-center items-center" >
-        <h1 className="font-Quintessential text-7xl text-center leading-normal">Code is how I think. <br /> Words are how I make you feel it.</h1>
+    <div id="home" className="min-h-dvh bg-offset-white md:px-[15vw]" >
+      <header>
+        <Navbar />
+      </header>
+      <main className="h-screen flex justify-center items-center border-red-500 border" >
+        <h1 className="font-great-vibes text-4xl md:text-7xl xl:text-8xl text-center leading-snug text-wrap px-5">Code is how I think. <br /> Words are how I make you feel it.</h1>
       </main>
-      <footer></footer>
+      <footer>
+
+      </footer>
     </div>
   )
 }
