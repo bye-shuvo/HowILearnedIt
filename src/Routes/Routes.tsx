f3fc37cccbe6b50c78825ebd9e71559d5fe858e4
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-05-12 01:46:46 +0600

    Update : Fixed things + added proper folder structure

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,7 +1,14 @@
-import {createBrowserRouter} from 'react-router-dom';
-import App from '../App.tsx';
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App.tsx";
+import Articles from "../Articles/Articles.tsx";
 
-export const routes = createBrowserRouter([{
-    path : "/",
-    element : <App />
-}])
+export const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/articles",
+    element: <Articles />,
+  },
+]);
