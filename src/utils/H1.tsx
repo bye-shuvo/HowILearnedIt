d072db8a7935df9723a7ebaef2298176244d6898
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-05-27 02:18:56 +0600

    Update : Added components for Article page

--- src/utils/H1.tsx
+++ src/utils/H1.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const H1 = ({heading}: {heading: string}) => {
+const H1 = ({heading}: {heading?: string}) => {
   return (
-      <h1 className="mb-5 laptop-lg:mb-10 text-md laptop-lg:text-xl flex items-center gap-3 w-full">
-        {heading} <p className="w-full h-px bg-text/20"></p>
+      <h1 className="mb-5 laptop-lg:mb-10 text-md laptop-lg:text-xl flex items-center gap-3 w-full text-text">
+        <p className="shrink-0">{heading}</p> <p className="w-full h-px bg-text/20"></p>
       </h1>
   )
 }
