659e22d90e873bf6a7f96ca4e1d9bf2af702f0c9
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-05-27 22:28:01 +0600

    Update : Added markdown processing in Content Component

--- src/Article/Content.tsx
+++ src/Article/Content.tsx
@@ -1,12 +1,56 @@
-import React from 'react'
-import type { Article } from '../Archive/types.ts';
+import { useEffect, useState } from "react";
+import type { Article } from "../Archive/types.ts";
+import { unified } from "unified";
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
+import rehypeStringify from "rehype-stringify";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeSanitize from "rehype-sanitize";
+import '../utils/markdown.css'
 
-const Content = ({article} : {article  : Article}) => {
+const Content = (_props: { article: Article }) => {
+  const [html, setHtml] = useState("");
+  useEffect(() => {
+    const processor = unified()
+      .use(remarkParse)
+      .use(remarkGfm)
+      .use(remarkRehype , {allowDangerousHtml: true})
+      .use(rehypeRaw)
+      .use(rehypeSanitize)
+      .use(rehypeStringify);
+
+    const value = `
+  
+  ## On discipline, restraint, and the architecture of thought
+  
+  Modern software systems often fail not because developers lack intelligence, but because they lack discipline.
+  
+  ## Why Simplicity Matters
+  
+  Simple systems are:
+  - easier to maintain
+  - easier to scale
+  - easier to debug
+
+  console.log("This is a code")
+  
+  ## Conclusion`;
+
+    processor.process(value).then((file) => {
+      setHtml(String(file));
+    });
+  }, []);
+
   return (
-    <section className="">
-      
+    <section className="w-full laptop-lg:py-10">
+      <div
+        id="markdown-content"
+        className="selection:bg-offset-white-hover"
+        dangerouslySetInnerHTML={{ __html: html }}
+      ></div>
     </section>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
