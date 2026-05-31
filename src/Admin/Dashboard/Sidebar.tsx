639cd38155c7c603823f6527e07dd1054ae70dc0
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-06-01 01:20:46 +0600

    Update : Changegit add .!

--- src/Admin/Dashboard/Sidebar.tsx
+++ src/Admin/Dashboard/Sidebar.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
-    <section id="sidebar" className="tablet-lg:p-5 flex flex-col items-center h-fit">
+    <section id="sidebar" className="sticky tablet-lg:top-10 tablet-lg:p-5 flex flex-col items-center laptop-lg:h-fit">
       <Link to={"/admin/dashboard"} className="py-2 hover:cursor-pointer hover:bg-offset-white-hover w-full text-center">Dashboard</Link>
       <Link to={"/admin/atricle/new"} className="py-2 hover:cursor-pointer hover:bg-offset-white-hover w-full text-center">Create New Article</Link>
       <Link to={"/admin/article/all"} className="py-2 hover:cursor-pointer hover:bg-offset-white-hover w-full text-center">All Articles</Link>
