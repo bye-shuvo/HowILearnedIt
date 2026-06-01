8540d41b4744fc14d03571a44e65e72ece15e1f5
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-06-02 00:50:16 +0600

    Update : Added responsiveness for dashboard page

--- src/Admin/Dashboard/Sidebar.tsx
+++ src/Admin/Dashboard/Sidebar.tsx
@@ -1,15 +1,78 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <section id="sidebar" className="sticky tablet-lg:top-10 tablet-lg:p-5 flex flex-col items-center laptop-lg:h-fit">
-      <Link to={"/admin/dashboard"} className="py-2 hover:cursor-pointer hover:bg-offset-white-hover w-full text-center">Dashboard</Link>
-      <Link to={"/admin/atricle/new"} className="py-2 hover:cursor-pointer hover:bg-offset-white-hover w-full text-center">Create New Article</Link>
-      <Link to={"/admin/article/all"} className="py-2 hover:cursor-pointer hover:bg-offset-white-hover w-full text-center">All Articles</Link>
-      <Link to={"/admin/Setting"} className="py-2 hover:cursor-pointer hover:bg-offset-white-hover w-full text-center">Settings</Link>
+    <section
+      id="sidebar"
+      className="sticky tablet-lg:top-10 tablet-lg:p-5 flex flex-col items-center laptop-lg:h-fit text-nowrap"
+    >
+      <Link
+        to={"/admin/dashboard"}
+        className="flex gap-3 items-center px-5 py-2 hover:cursor-pointer hover:bg-offset-white-hover w-full"
+      >
+        <svg
+          className="h-5 w-5"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 384 512"
+        >
+          <path
+            className="fill-text"
+            d="M311.4 32l8.6 0c35.3 0 64 28.7 64 64l0 352c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 96C0 60.7 28.7 32 64 32l8.6 0C83.6 12.9 104.3 0 128 0L256 0c23.7 0 44.4 12.9 55.4 32zM248 112c13.3 0 24-10.7 24-24s-10.7-24-24-24L136 64c-13.3 0-24 10.7-24 24s10.7 24 24 24l112 0zM128 256a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm32 0c0 13.3 10.7 24 24 24l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-112 0c-13.3 0-24 10.7-24 24zm0 128c0 13.3 10.7 24 24 24l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-112 0c-13.3 0-24 10.7-24 24zM96 416a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"
+          />
+        </svg>
+        Dashboard
+      </Link>
+      <Link
+        to={"/admin/atricle/new"}
+        className="flex gap-3 items-center px-5 py-2 hover:cursor-pointer hover:bg-offset-white-hover w-full"
+      >
+        <svg
+          className="h-5 w-5"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 576 512"
+        >
+          <path
+            className="fill-text"
+            d="M96 0C60.7 0 32 28.7 32 64l0 384c0 35.3 28.7 64 64 64l180 0c-22.7-31.5-36-70.2-36-112 0-100.6 77.4-183.2 176-191.3l0-38.1c0-17-6.7-33.3-18.7-45.3L290.7 18.7C278.7 6.7 262.5 0 245.5 0L96 0zM357.5 176L264 176c-13.3 0-24-10.7-24-24L240 58.5 357.5 176zM432 544a144 144 0 1 0 0-288 144 144 0 1 0 0 288zm16-208l0 48 48 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-48 0 0 48c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-48-48 0c-8.8 0-16-7.2-16-16s7.2-16 16-16l48 0 0-48c0-8.8 7.2-16 16-16s16 7.2 16 16z"
+          />
+        </svg>
+        Create New Article
+      </Link>
+      <Link
+        to={"/admin/article/all"}
+        className="flex gap-3 items-center px-5 py-2 hover:cursor-pointer hover:bg-offset-white-hover w-full"
+      >
+        <svg
+          className="h-5 w-5"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <path
+            className="fill-text"
+            d="M0 416L0 120c0-13.3 10.7-24 24-24s24 10.7 24 24l0 288c0 13.3 10.7 24 24 24s24-10.7 24-24L96 96c0-35.3 28.7-64 64-64l288 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64zM160 128l0 64c0 17.7 14.3 32 32 32l64 0c17.7 0 32-14.3 32-32l0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32zm24 240c-13.3 0-24 10.7-24 24s10.7 24 24 24l240 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-240 0zm-24-72c0 13.3 10.7 24 24 24l240 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-240 0c-13.3 0-24 10.7-24 24zM360 176c-13.3 0-24 10.7-24 24s10.7 24 24 24l64 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-64 0z"
+          />
+        </svg>
+        All Articles
+      </Link>
+      <Link
+        to={"/admin/Setting"}
+        className="flex gap-3 items-center px-5 py-2 hover:cursor-pointer hover:bg-offset-white-hover w-full"
+      >
+        <svg
+          className="h-5 w-5"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <path
+            className="fill-text"
+            d="M195.1 9.5C198.1-5.3 211.2-16 226.4-16l59.8 0c15.2 0 28.3 10.7 31.3 25.5L332 79.5c14.1 6 27.3 13.7 39.3 22.8l67.8-22.5c14.4-4.8 30.2 1.2 37.8 14.4l29.9 51.8c7.6 13.2 4.9 29.8-6.5 39.9L447 233.3c.9 7.4 1.3 15 1.3 22.7s-.5 15.3-1.3 22.7l53.4 47.5c11.4 10.1 14 26.8 6.5 39.9l-29.9 51.8c-7.6 13.1-23.4 19.2-37.8 14.4l-67.8-22.5c-12.1 9.1-25.3 16.7-39.3 22.8l-14.4 69.9c-3.1 14.9-16.2 25.5-31.3 25.5l-59.8 0c-15.2 0-28.3-10.7-31.3-25.5l-14.4-69.9c-14.1-6-27.2-13.7-39.3-22.8L73.5 432.3c-14.4 4.8-30.2-1.2-37.8-14.4L5.8 366.1c-7.6-13.2-4.9-29.8 6.5-39.9l53.4-47.5c-.9-7.4-1.3-15-1.3-22.7s.5-15.3 1.3-22.7L12.3 185.8c-11.4-10.1-14-26.8-6.5-39.9L35.7 94.1c7.6-13.2 23.4-19.2 37.8-14.4l67.8 22.5c12.1-9.1 25.3-16.7 39.3-22.8L195.1 9.5zM256.3 336a80 80 0 1 0 -.6-160 80 80 0 1 0 .6 160z"
+          />
+        </svg>
+        Settings
+      </Link>
     </section>
-  )
-}
+  );
+};
 
-export default Sidebar ;
+export default Sidebar;
