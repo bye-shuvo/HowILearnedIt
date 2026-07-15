d5a5b004d42c2ef407fc4784857cd4799299a9bd
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-07-15 23:51:36 +0600

    Update: Unfinished task

--- src/Admin/New Article/utils/ContentEditor.tsx
+++ src/Admin/New Article/utils/ContentEditor.tsx
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import type { articleInput } from "./input";
 import ContentController from "./ContentController";
 
 const ContentEditor = ({ input }: { input: articleInput }) => {
+  const contentRef = useRef<HTMLDivElement | null>(null);
   return (
-    <div id="content-editor" className="flex flex-col gap-3">
-      <div aria-label="Content-write" className="flex w-full h-fit min-h-40 ring-1 relative">
-        <textarea
-          name={input.fieldName}
+    <div id={input.fieldName} className="flex flex-col gap-3">
+      <div
+        aria-label="Content-write"
+        className="flex w-full h-fit pb-14 ring-1 relative scrollbar-thumb-text/50 scrollbar-track-offset-white"
+      >
+        <div
+          ref={contentRef}
           id={input.fieldName}
-          rows={15}
-          aria-rowcount={15}
-          className="text-md w-full outline-0 p-4"
-        ></textarea>
-        <ContentController />
+          role="textbox"
+          aria-label={input.labelName}
+          className="text-[17px] w-full min-h-100 outline-0 p-4"
+          contentEditable={true}
+          translate="no"
+        />
+        <ContentController
+          contentRef = {contentRef}
+        />
       </div>
-      <button aria-label="Content-rephraser" className="ring-1 py-2 bg-offset-white-hover/90 hover:text-white hover:cursor-pointer hover:bg-text">
+      <button
+        aria-label="Content-rephraser"
+        className="ring-1 py-2 bg-offset-white-hover/90 hover:text-white hover:cursor-pointer hover:bg-text"
+      >
         Rephrase
       </button>
     </div>
